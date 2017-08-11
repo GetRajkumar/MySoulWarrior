151cc9a31452244ae49843a21a8b697412b11036
Rajkumar Subramanian <rajs.4391@gmail.com>
2017-08-12 01:08:04 +0530

    js new changes

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,23 @@
   
 
 */
+// spy and scroll menu boogey
+$("#mainMenu ul li a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault()
+
+   // store hash
+   var hash = this.hash
+
+   // animate
+    var w = window.innerWidth;
+   // if(w > 1000){
+   $('html, body').animate({   
+       scrollTop: $(this.hash).offset().top -50 
+     }, 1000, function(){
+       window.location.hash = hash
+     })
+   // }
+
+});
